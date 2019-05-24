d4176f90ec318125d6f847a696fd76e74f578644
ArtPoon <artpoon@gmail.com>
2019-05-23 23:27:49 -0400

    got ship1 moving

--- js/spacewar.js
+++ js/spacewar.js
@@ -1,28 +1,123 @@
 // module aliases
+Matter.use('matter-wrap');
+
 var Engine = Matter.Engine,
     Render = Matter.Render,
+    Runner = Matter.Runner,
+    Events = Matter.Events,
+    Composites = Matter.Composites,
+    Common = Matter.Common,
+    MouseConstraint = Matter.MouseConstraint,
+    Mouse = Matter.Mouse,
     World = Matter.World,
+    Body = Matter.Body,
     Bodies = Matter.Bodies;
 
 // create an engine
-var engine = Engine.create();
+var engine = Engine.create(),
+    world = engine.world;
 
-// create a renderer
+engine.world.gravity.y = 0;
+
+// create renderer
 var render = Render.create({
     element: document.body,
-    engine: engine
+    engine: engine,
+    options: {
+        width: 800,
+        height: 600,
+        showVelocity: true,
+        showAngleIndicator: true
+    }
 });
 
-// create two boxes and a ground
-var boxA = Bodies.rectangle(400, 200, 80, 80);
-var boxB = Bodies.rectangle(450, 50, 80, 80);
-var ground = Bodies.rectangle(400, 610, 810, 60, { isStatic: true });
-
-// add all of the bodies to the world
-World.add(engine.world, [boxA, boxB, ground]);
-
-// run the engine
-Engine.run(engine);
-
-// run the renderer
 Render.run(render);
+
+
+
+
+
+
+// create two ships
+function makeaship() {
+    return Bodies.circle(
+        Common.random(0, render.options.width),
+        Common.random(0, render.options.height),
+        20,
+        {
+            restitution: 0.99,  // bounce
+            friction: 0,
+            frictionAir: 0,
+            // set the body's wrapping bounds
+            plugin: {
+                wrap: {
+                    min: {x: 0, y: 0},
+                    max: {x: render.canvas.width, y: render.canvas.height}
+                }
+            }
+        }
+    );
+}
+var ship1 = makeaship(),
+    ship2 = makeaship();
+
+World.add(world, [ship1, ship2]);
+
+
+
+
+// looks for key presses and logs them
+// https://gist.github.com/lilgreenland/c6f4b78a78b73dc8b1f8fa650d617b85
+var keys = [];
+document.body.addEventListener("keydown", function(e) {
+    keys[e.keyCode] = true;
+});
+document.body.addEventListener("keyup", function(e) {
+    keys[e.keyCode] = false;
+});
+
+Events.on(engine, "beforeTick", function(event) {
+    if (keys[37]) {
+        // left arrow
+        Body.rotate(ship1, -0.1);
+    }
+    if (keys[39]) {
+        // right arrow
+        Body.rotate(ship1, 0.1);
+    }
+    if (keys[38]) {
+        // up arrow
+        Body.applyForce(ship1,
+            {x: ship1.position.x, y: ship1.position.y},
+            {x: Math.cos(ship1.angle) * 0.001, y: Math.sin(ship1.angle) * 0.001}
+            )
+    }
+});
+
+
+
+var runner = Runner.create();
+Runner.run(runner, engine);
+
+
+
+
+// make Bodies draggable
+var mouse = Mouse.create(render.canvas),
+    mouseConstraint = MouseConstraint.create(engine, {
+        mouse: mouse,
+        constraint: {
+            stiffness: 0.2,  // of spring
+            render: {
+                visible: false
+            }
+        }
+    });
+
+World.add(world, mouseConstraint);
+
+// keep the mouse in sync with rendering
+render.mouse = mouse;
+
+
+
